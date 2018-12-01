7e3b47701e6fede965023bbf4684e8cbeb43a186
Michael L Parks <github@wmi-inc.com>
2018-12-01 12:01:26 -0500

    Add Random Bomb Generator Functionality

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -11,8 +11,52 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
         board.push(row);
     }
     return board;
-}
+};
 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    //set generateBombBoard equal to an arrow function that 
+    //takes three parameters: numberOfRows, numberOfColumns, numberOfBombs.
+    let board = [];
+    for( let r = 0; r < numberOfRows; r++){
+        row = [];
+        for(let c = 0; c < numberOfColumns; c++){
+            row.push(null);
+        }
+        board.push(row);
+    }
+
+    let numberOfBombsPlaced = 0;
+    while(numberOfBombsPlaced < numberOfBombs){
+        /* while loop has the potential to place bombs on top of 
+        already existing bombs. This will be fixed when you learn 
+        about control flow.*/
+
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows );
+
+        // use the board variable and access the space at the generated 
+        // randomRowIndex and randomColumnIndex. Set it equal to 'B'.
+
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns );
+
+        board[randomRowIndex][randomColumnIndex] = 'B';
+
+        numberOfBombsPlaced++;
+
+    }
+    return board;
+};
+
+const printBoard = (board) => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+}
 // console.log(generatePlayerBoard(3,3));
 // console.log(generatePlayerBoard(5,5));
 // console.log(generatePlayerBoard(10,10));
+
+console.log('Player Board: ');
+let playerBoard = generatePlayerBoard(3, 4);
+console.log(printBoard(playerBoard));
+
+console.log('Bomb Board: ');
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log(printBoard(bombBoard));
